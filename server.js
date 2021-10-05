3d03638f94d60b15c236f7bba7b19ecc4f5a0d6b
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2021-10-05 12:08:28 +0100

    Adding comments and tidying content

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-//Server for API call to database
-//@author Rohith Ravindranath
-//@version July 10 2019
+//Server for API call to Db2 database
+//@author Andrew Laidlaw
+//@version 1.0
 
-// require('dotenv').config()
 var express=require('express');
 var app=express();
 var path=require('path');
@@ -24,14 +23,6 @@ const DB_PORT = process.env.db_port;
 const DB_UID = process.env.db_uid;
 const DB_PWD = process.env.db_pwd;
 const DB_SCHEMA = process.env.db_schema;
-
-// Test values
-//const DB_DATABASE=SAMPLE
-//const DB_HOSTNAME=p1284-pvm1.p1284.cecc.ihost.com
-//const DB_PORT=50000
-//const DB_UID=cecuser
-//const DB_PWD=BXUMWPH-VA%R6Mb
-//const DB_SCHEMA=CECUSER
 
 // Setup the endpoint for the Db2 database we are connecting to
 let connStr = "DATABASE="+process.env.DB_DATABASE+";HOSTNAME="+process.env.DB_HOSTNAME+";PORT="+process.env.DB_PORT+";PROTOCOL=TCPIP;UID="+process.env.DB_UID+";PWD="+process.env.DB_PWD+";";
@@ -70,6 +61,7 @@ app.get('/getProducts', function(request, response) {
 })
 
 // Get an object containing the full details of a particular employee identified by their ID
+// Request should be of form <hostname>:8080/getEmployee?id=<employee ID>
 app.get('/getEmployee', function(request, response) {
   console.log("Request for /getEmployee with Employee Number "+request.query.id);
   ibmdb.open(connStr, function (err,conn) {
@@ -91,6 +83,7 @@ app.get('/getEmployee', function(request, response) {
 })
 
 // Get an object containing limited details of all employees in the database
+// Returns the employee number, first name, last name, and job title of each employee
 app.get('/getEmployees', function(request, response) {
   console.log("Request for /getEmployees");
   ibmdb.open(connStr, function (err,conn) {
@@ -111,7 +104,32 @@ app.get('/getEmployees', function(request, response) {
   })
 })
 
+
 // Get an object containing limited details of all employees in the database
+// Returns the employee number, first name, last name, job title, and department name of each employee
+app.get('/getAllEmployees', function(request, response) {
+  console.log("Request for /getAllEmployees");
+  ibmdb.open(connStr, function (err,conn) {
+    if (err){
+      console.log(err);
+      return response.json({success:-1, message:err});
+    }
+    conn.query("SELECT e.EMPNO,e.FIRSTNME,e.LASTNAME,e.JOB,d.DEPTNAME FROM "+process.env.DB_SCHEMA+".EMPLOYEE e INNER JOIN "+process.env.DB_SCHEMA+".DEPARTMENT d ON e.WORKDEPT = d.DEPTNO;", function (err,data) {
+      if (err){
+        console.log(err);
+        return response.json({success:-2,message:err});
+      }
+      conn.close(function () {
+        console.log("Response provided");
+        return response.json({success:1, message:'Data Received!', data:data});
+      });
+    })
+  })
+})
+
+// Get an object containing limited details of all employees in the database
+// Returns the employee number, first name, last name, and job title of each employee
+// This is a synchronous call on the database
 app.get('/getEmps', function(request, response) {
   console.log("Request for /getEmps");
   ibmdb.open(connStr, function (err,conn) {
@@ -126,176 +144,6 @@ app.get('/getEmps', function(request, response) {
       return response.json({success:1, message:'Data Received!', data:output});
     })
   })
-})
-
-// The following endpoints are historical from the cloned Github repo, and are not used here
-
- app.post('/newDataEntry', function(request, response){
-   var house = JSON.parse(request.body['house']);
-
-    ibmdb.open(connStr, function (err,conn) {
-      if (err){
-        return response.json({success:-1, message:err});
-      }
-      conn.query("SELECT MAX(ID) FROM "+process.env.DB_SCHEMA+".HOME_SALES;", function (err, data) {
-        if (err){
-          return response.json({success:-2, message:err});
-        }
-        else{
-          var id = data[0]['1'] + 1;
-          var address_info = JSON.parse(request.body['address']);
-
-          var str = "INSERT INTO "+process.env.DB_SCHEMA+".HOME_ADDRESS (ADDRESS1, ADDRESS2, CITY, STATE,ZIPCODE, COUNTRY,HOME_ID) VALUES ('"+address_info['address1']+"', '"+address_info['address2']+"', '"+address_info['city']+"', '"+address_info['state']+"', "+address_info['zipcode']+", '"+address_info['country']+"', "+id+");";
-
-          var s = "INSERT INTO "+process.env.DB_SCHEMA+".HOME_SALES (ID,LOTAREA, YEARBUILT, BLDGTYPE,HOUSESTYLE,OVERALLCOND,ROOFSTYLE,EXTERCOND,FOUNDATION,BSMTCOND,HEATING,HEATINGQC,CENTRALAIR,ELECTRICAL,FULLBATH,HALFBATH,BEDROOMABVGR,KITCHENABVGR,KITCHENQUAL,TOTRMSABVGRD,FIREPLACES,FIREPLACEQU,GARAGETYPE,GARAGEFINISH,GARAGECARS,GARAGECOND,POOLAREA,POOLQC,FENCE,MOSOLD,YRSOLD,SALEPRICE ) VALUES ("+id+","+house['lotArea']+","+house['yearBuilt']+",'"+house['bldgType']+"','"+house['houseStyle']+"',"+house['overallCond']+",'"+house['roofStyle']+"','"+house['exterCond']+"','"+house['foundation']+"','"+house['bsmtCond']+"','"+house['heating']+"','"+house['heatingQC']+"','"+house['centralAir']+"','"+house['electrical']+"',"+house['fullBath']+","+house['halfBath']+","+house['bedroomAbvGr']+","+house['kitchenAbvGr']+",'"+house['kitchenQual']+"',"+house['tempotRmsAbvGrd']+","+house['fireplaces']+",'"+house['fireplaceQu']+"','"+house['garageType']+"','"+house['garageFinish']+"',"+house['garageCars']+",'"+house['garageCond']+"',"+house['poolArea']+",'"+house['poolQC']+"','"+house['fence']+"',"+house['moSold']+","+house['yrSold']+","+house['salePrice']+");";
-          conn.query(s, function (err, data) {
-            if (err){
-              return response.json({success:-3, message:err});
-            }
-            else{
-              conn.query(str, function (err, data) {
-                if (err){
-                  return response.json({success:-4, message:err});
-                }
-                else{
-                  conn.close(function () {
-                    return response.json({success:1, message:'Data Entered!'});
-                  });
-                }
-              });
-            }
-          });
-        }
-      });
-    });
-
-})
-
-app.post('/getData', function(request, response){
-  console.log('hi')
-   ibmdb.open(connStr, function (err,conn) {
-     if (err){
-       return response.json({success:-1, message:err});
-     }
-     conn.query("SELECT * FROM "+process.env.DB_SCHEMA+".HOME_SALES ORDER BY ID DESC LIMIT "+request.body.num+";", function (err, data) {
-       if (err){
-         return response.json({success:-2, message:err});
-       }
-       conn.close(function () {
-         return response.json({success:1, message:'Data Received!', data:data});
-       });
-     });
-   });
-})
-
-app.post('/getUniqueData', function(request, response){
-   ibmdb.open(connStr, function (err,conn) {
-     if (err){
-       return response.json({success:-1, message:err});
-     }
-     conn.query("SELECT * FROM "+process.env.DB_SCHEMA+".HOME_SALES WHERE ID="+request.body.id+";", function (err, data) {
-       if (err){
-         return response.json({success:-2, message:err});
-       }
-       else{
-         conn.query("SELECT * FROM "+process.env.DB_SCHEMA+".HOME_ADDRESS WHERE HOME_ID="+request.body.id+";", function (err, data2) {
-           if (err){
-             return response.json({success:-3, message:err});
-           }
-           conn.close(function () {
-             console.log(data);
-             console.log(data2.length);
-             if (data2.length == 0){
-               data2[0] = {'ADDRESS1': '', 'ADDRESS2': '','CITY': '','STATE': '','COUNTRY': '','ZIPCODE': '','HOME_ID': data[0]['ID']};
-               console.log(data2);
-             }
-
-             return response.json({success:1, message:'Data Received!', data:data,data2:data2 });
-           });
-         });
-       }
-     });
-   });
-})
-
-app.post('/updateDataEntry', function(request, response){
-  ibmdb.open(connStr, function (err,conn) {
-    if (err){
-      return response.json({success:-1, message:err});
-    }
-
-    var str2 = "UPDATE "+process.env.DB_SCHEMA+".HOME_ADDRESS SET ADDRESS1='"+request.body.addressInfo.address1+"',ADDRESS2='"+request.body.addressInfo.address2+"',CITY='"+request.body.addressInfo.city+"',STATE='"+request.body.addressInfo.state+"',COUNTRY='"+request.body.addressInfo.country+"',ZIPCODE="+request.body.addressInfo.zipcode+" WHERE HOME_ID="+request.body.id+";";
-
-    var str4 = "INSERT INTO "+process.env.DB_SCHEMA+".HOME_ADDRESS (ADDRESS1, ADDRESS2, CITY, STATE,ZIPCODE, COUNTRY,HOME_ID) VALUES ('"+request.body.addressInfo.address1+"', '"+request.body.addressInfo.address2+"', '"+request.body.addressInfo.city+"', '"+request.body.addressInfo.state+"', "+request.body.addressInfo.zipcode+", '"+request.body.addressInfo.country+"', "+request.body.id+");";
-
-    var str = "UPDATE "+process.env.DB_SCHEMA+".HOME_SALES SET LOTAREA="+request.body.data.lotArea+", YEARBUILT="+request.body.data.yearBuilt+", BLDGTYPE='"+request.body.data.bldgType+"',HOUSESTYLE='"+request.body.data.houseStyle+"',OVERALLCOND="+request.body.data.overallCond+",ROOFSTYLE='"+request.body.data.roofStyle+"',EXTERCOND='"+request.body.data.exterCond+"',FOUNDATION='"+request.body.data.foundation+"',BSMTCOND='"+request.body.data.bsmtCond+"',HEATING='"+request.body.data.heating+"',HEATINGQC='"+request.body.data.heatingQC+"',CENTRALAIR='"+request.body.data.centralAir+"',ELECTRICAL='"+request.body.data.electrical+"',FULLBATH="+request.body.data.fullBath+",HALFBATH="+request.body.data.halfBath+",BEDROOMABVGR="+request.body.data.bedroomAbvGr+",KITCHENABVGR="+request.body.data.kitchenAbvGr+",KITCHENQUAL='"+request.body.data.kitchenQual+"',TOTRMSABVGRD="+request.body.data.tempotRmsAbvGrd+",FIREPLACES="+request.body.data.fireplaces+",FIREPLACEQU='"+request.body.data.fireplaceQu+"',GARAGETYPE='"+request.body.data.garageType+"',GARAGEFINISH='"+request.body.data.garageFinish+"',GARAGECARS="+request.body.data.garageCars+",GARAGECOND='"+request.body.data.garageCond+"',POOLAREA="+request.body.data.poolArea+",POOLQC='"+request.body.data.poolQC+"',FENCE='"+request.body.data.fence+"',MOSOLD="+request.body.data.moSold+",YRSOLD="+request.body.data.yrSold+",SALEPRICE="+request.body.data.salePrice+" WHERE ID="+request.body.id+";";
-
-    var str3 = "SELECT * FROM "+process.env.DB_SCHEMA+".HOME_ADDRESS WHERE HOME_ID="+request.body.id + ";";
-
-    conn.query(str, function (err, data) {
-      if (err){
-        return response.json({success:-2, message:err});
-      }
-      conn.query(str3, function (err, data2) {
-        console.log(data);
-        if (err){
-          return response.json({success:-3, message:err});
-        }
-        else{
-          if (data2.length == 0 ){
-            conn.query(str4, function (err, data) {
-              if (err){
-                return response.json({success:-2, message:err});
-              }
-              else{
-                conn.close(function () {
-                  return response.json({success:1, message:'Data Edited!'});
-                });
-              }
-            });
-          }
-          else{
-            conn.query(str2, function (err, data) {
-              if (err){
-                return response.json({success:-2, message:err});
-              }
-              else{
-                conn.close(function () {
-                  return response.json({success:1, message:'Data Edited!'});
-                });
-              }
-            });
-          }
-        }
-
-      });
-    });
-  });
-})
-
-
-app.post('/deleteData', function(request, response){
-   ibmdb.open(connStr, function (err,conn) {
-     if (err){
-       return response.json({success:-1, message:err});
-     }
-     conn.query("DELETE FROM "+process.env.DB_SCHEMA+".HOME_SALES WHERE ID="+request.body.id+";", function (err, data) {
-       if (err){
-         return response.json({success:-2, message:err});
-       }
-       else{
-         conn.query(" DELETE FROM "+process.env.DB_SCHEMA+".HOME_ADDRESS WHERE HOME_ID="+request.body.id+";", function (err, data) {
-           if (err){
-             return response.json({success:-3, message:err});
-           }
-           conn.close(function () {
-             return response.json({success:1, message:'Data Deleted!'});
-           });
-         });
-       }
-
-     });
-   });
 })
 
 // Start the server listening for clients
